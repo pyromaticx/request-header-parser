35afa1a4f1842f44d4fa4727f71022b5ab473705
Kelly Garrett <krginvest@gmail.com>
2016-01-09 18:18:38 -0800

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname));
+
+app.get('/', function(req, res) {
+  var clientInfo = {
+    ip: req.hostname,
+    language: req.headers['accept-language'].slice(0,5),
+    software: req.headers['user-agent']
+  }
+  console.log(clientInfo);
+  res.send(JSON.stringify(clientInfo));
+});
+
+app.listen(port);
