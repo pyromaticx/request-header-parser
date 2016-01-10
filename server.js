6b006faba0f5324d1173737020076a4f510482b3
Kelly Garrett <krginvest@gmail.com>
2016-01-09 18:39:13 -0800

    changed server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.static(__dirname));
 
 app.get('/', function(req, res) {
   var clientInfo = {
-    ip: req.ip,
+    ip: req.headers['x-forwarded-for'] || req.connection.remoteAddress,
     language: req.headers['accept-language'].slice(0,5),
     software: req.headers['user-agent']
   }
